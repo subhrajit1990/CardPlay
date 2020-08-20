e7a0ce646465cc0d1106a3ab9e3731308259c47f
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2020-08-20 15:47:34 +0530

    Add files via upload
    Creating folders

---
+++ js/gameStarts.js
@@ -0,0 +1,14 @@
+
+if (typeof (SUBU) == "undefined") {
+    SUBU = {}
+}
+
+SUBU.CardPlayStarts = new function(){
+	this.init = function(){
+		var counter;
+		counter = new cardPlay();
+		counter.init("timer", 180); // expects in seconds
+	}
+}
+
+su_cardPlay = SUBU.CardPlayStarts;
